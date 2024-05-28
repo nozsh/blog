16647d6ce062acd44902aacb2271cf6e261a153b
nozsh <155148075+nozsh@users.noreply.github.com>
2024-05-28 21:07:35 +0300

    upd

--- push.sh
+++ push.sh
@@ -4,6 +4,10 @@ echo "- Purging public dir"
 echo
 rm -rf public/* && echo "- Purging public dir: Done"
 echo
+echo "- Purgins gen dir (resources/_gen/images)"
+echo
+rm -rf resources/_gen/images/* && echo "- Purging resource/_gen/images dir: Done"
+echo
 echo "- Re-generate public dir"
 echo
 hugo && echo "Re-generate public dir: Done"
